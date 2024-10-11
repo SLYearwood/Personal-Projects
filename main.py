e1e8db5f117d0b6527324b5c05be354cd5ce419b
SLYearwood <slyearwood7@gmail.com>
2024-10-11 16:02:22 -0400

    Add file saving and loading functionality using JSON

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from finance_tracker import FinanceTracker
 
+# main.py (update the menu)
+
 def main():
     tracker = FinanceTracker()
     while True:
         print("\n--- Personal Finance Tracker ---")
         print("1. Add Transaction")
         print("2. View Transactions")
-        print("3. Exit")
+        print("3. Save Transactions")
+        print("4. Load Transactions")
+        print("5. Exit")
         choice = input("Select an option: ")
         
         if choice == '1':
@@ -17,10 +21,14 @@ def main():
         elif choice == '2':
             tracker.view_transactions()
         elif choice == '3':
+            filename = input("Enter filename to save: ")
+            tracker.save_transactions(filename)
+        elif choice == '4':
+            filename = input("Enter filename to load: ")
+            tracker.load_transactions(filename)
+        elif choice == '5':
             print("Exiting... Goodbye!")
             break
         else:
             print("Invalid option! Please try again.")
 
-if __name__ == "__main__":
-    main()
