07508a71e185e732b015797dbc780eb7e5003206
SLYearwood <slyearwood7@gmail.com>
2024-10-11 16:00:19 -0400

    Add save_transactions function

--- finance_tracker.py
+++ finance_tracker.py
@@ -1,4 +1,5 @@
 from transaction import Transcation
+import json
 
 class FinanceTracker:
     def __init__(self):
@@ -19,3 +20,9 @@ class FinanceTracker:
         for t in self.transactions:
             print(f"{t.date} - {t.transaction_type.capitalize()}: ${t.amount} | Category: {t.category}")
         print(f"\nCurrent Balance: ${self.balance:.2f}\n")
+
+    def save_transactions(self, filename):
+        data = [{'amount': t.amount, 'category': t.category, 'transaction_type': t.transaction_type, 'date': t.date} for t in self.transactions]
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+        print(f"Transactions saved to {filename}")
