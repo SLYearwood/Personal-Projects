8707c6663b745ba873a814bdd9a100335db22c21
SLYearwood <slyearwood7@gmail.com>
2024-10-11 15:52:43 -0400

    Create add_transaction function

--- finance_tracker.py
+++ finance_tracker.py
@@ -5,3 +5,11 @@ class FinanceTracker:
         self.transactions = []
         self.balance = 0.0
     
+    def add_transaction(self, amount, category, transaction_type):
+        transaction = Transcation(amount, category, transaction_type)
+        self.transactions.append(transaction)
+        if transaction_type == 'income':
+            self.balance += amount
+        elif transaction_type == 'expense':
+            self.balance -= amount
+        print(f"Transaction added: {transaction_type} of {amount} for {category}")
